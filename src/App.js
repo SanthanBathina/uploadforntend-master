2c93d5d3b579ab62a69dbe61cadb1750e0b1a50d
SanthanBathina <bvsanthan@gmail.com>
2024-10-21 08:51:03 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import FileUpload from './FileUpload';
+import FileRetrieval from './FileRetrieval';
+
+function App() {
+    return (
+        <div className="App">
+            <FileUpload />
+            <FileRetrieval />
+        </div>
+    );
+}
+
+export default App;
